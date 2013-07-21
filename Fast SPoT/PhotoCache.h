a3b547446bf5f5f9ad33134742d77a051c4aebd8
Richard Shin <skunkworks@gmail.com>
2013-07-21 00:28:27 -0700

    Formatting & comments

--- Fast SPoT/PhotoCache.h
+++ Fast SPoT/PhotoCache.h
@@ -10,13 +10,18 @@
 
 @interface PhotoCache : NSObject
 
-+ (PhotoCache *)sharedInstance;
-
+// Set up the cache with a certain capacity. Setting persists between app launches
 @property (nonatomic) NSUInteger capacity;
 
+// Access singleton instance
++ (PhotoCache *)sharedInstance;
+
+// Add a photo to the cache
 - (void)addPhoto:(UIImage *)photo fromURL:(NSURL *)url;
 
+// Retrieves a photo from the cache. Returns nil on cache miss
 - (UIImage *)retrievePhotoForURL:(NSURL *)url;
 
+// Clear out the cache
 - (void)clear;
 @end
